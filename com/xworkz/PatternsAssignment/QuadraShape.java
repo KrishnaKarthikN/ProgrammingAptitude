2d10be53499ae799f83b43cfb37454c28a3be53a
Krishna Karthik N <71656457+KrishnaKarthikN@users.noreply.github.com>
2020-12-16 11:25:01 +0530

    Commit 1

---
+++ com/xworkz/PatternsAssignment/QuadraShape.java
@@ -0,0 +1,22 @@
+package com.xworkz.PatternsAssignment;
+
+public class QuadraShape {
+
+public static void main(String[] args) {
+		
+		int num = 4;
+		for (int i= 0; i<=num; i++)
+        {
+            for (int j=0; j<=num; j++)
+            {
+                if(i==0 || i==num || j==0 || j==num )
+                	System.out.print("*");
+                else 
+                	System.out.print(" ");
+            }
+		System.out.println();
+        }
+		
+	}
+	
+}
---
+++ com/xworkz/primeNumbers/PrimeNumbers.java
@@ -0,0 +1,21 @@
+package com.xworkz.primeNumbers;
+
+public class PrimeNumbers {
+
+	public static void main(String[] args) {
+		int  num = 59 ;
+		int count = 1;
+		for ( int i = 2 ; i < num ; i++ ) {
+			if(num % i == 0 ) {
+				count = 0;
+			break;
+			}
+		}
+		if (count==1)
+			System.out.println(num + " Number is prime");
+		else 
+			System.out.println(num + " number is not prime");
+		
+	}
+
+}
