165ff386cce3dd6b982fc3dc5d70be3706e33608
Krishna Karthik N <71656457+KrishnaKarthikN@users.noreply.github.com>
2020-12-16 11:45:51 +0530

    Commit 1

---
+++ com/xworkz/primeNumbers/PrimeNumbersUsingWhileLoop.java
@@ -0,0 +1,23 @@
+package com.xworkz.primeNumbers;
+
+public class PrimeNumbersUsingWhileLoop {
+
+	public static void main(String[] args) {
+		int  num = 59 ;
+		int i = 2;
+		int count = 1;
+		while(i < num && num%i ==0 ) {
+			count = 0;
+			i++;
+			break;
+		}
+		if (count==1)
+			System.out.println(num + " - Number is prime");
+		else 
+			System.out.println(num + " - Number is not prime");
+		
+	}
+
+	
+	
+}
